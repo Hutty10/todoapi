c726d2515802c8dd1547cbf57c421d94794e230e
Uthman <opedayo81@gmail.com>
2022-06-04 19:56:03 +0100

    changed the status to boolean field

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -7,16 +8,11 @@ from account.models import User
 
 
 class Todo(models.Model):
-    choice = (
-        ('created', 'created'),
-        ('doing', 'doing'),
-        ('done', 'done'),
-    )
 
     owner = models.ForeignKey(User, verbose_name=_('owner'), on_delete=models.CASCADE)
     title = models.CharField(verbose_name=_('Todo title'), max_length=150)
     description = models.TextField(verbose_name=_('Todo description'), blank=True)
-    status = models.CharField(verbose_name=_('Status'), default='created', max_length=7)
+    status = models.models.BooleanField(_("Status"), default=False)
     start = models.DateTimeField(verbose_name=_('Start date'))
     updated = models.DateTimeField(_("Date Updated"), auto_now=True)
     created = models.DateTimeField(verbose_name=_('Date created'), auto_now_add=True)
