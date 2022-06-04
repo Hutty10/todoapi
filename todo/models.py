c45747a949adbcb5c1d55a830f08ccaf0fe28760
Uthman <opedayo81@gmail.com>
2022-06-04 19:57:24 +0100

    fixed mistake

--- todo/models.py
+++ todo/models.py
@@ -12,7 +12,7 @@ class Todo(models.Model):
     owner = models.ForeignKey(User, verbose_name=_('owner'), on_delete=models.CASCADE)
     title = models.CharField(verbose_name=_('Todo title'), max_length=150)
     description = models.TextField(verbose_name=_('Todo description'), blank=True)
-    status = models.models.BooleanField(_("Status"), default=False)
+    status = models.BooleanField(_("Status"), default=False)
     start = models.DateTimeField(verbose_name=_('Start date'))
     updated = models.DateTimeField(_("Date Updated"), auto_now=True)
     created = models.DateTimeField(verbose_name=_('Date created'), auto_now_add=True)
